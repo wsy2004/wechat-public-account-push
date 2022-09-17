8813fbf70d9808bd769e31325da253b6edd526e4
wsy2004 <113604964+wsy2004@users.noreply.github.com>
2022-09-17 20:26:56 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "身边的大肚皮",
+    APP_ID: "wx17dcc1bd529bd454",
     // 公众号APP_SECRET
-    APP_SECRET: "FF521",
+    APP_SECRET: "9917a0eddfd045f21ab467a902ff1309",
     // 模板消息id
-    TEMPLATE_ID: "	gciRxRu9ZRzniIERv2QJYxgaIy400ml5oYkNyRs9NBk",
+    TEMPLATE_ID: "	Ot-O2SEGc1uuAgTIRaLERpADzcQ1wsqEalZIp58Othg",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: ["wsy210803"],
+    USERS: ["oW8om6AQ5Sh9zkKTssbuo5zc49PU"],
      
     /**
      * 信息配置
@@ -26,7 +26,7 @@ export const config = {
     /** 生日相关 */
 
     // 生日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
-    BIRTHDAYS: [2005-02-06
+    BIRTHDAYS: [02-06
       {"name": "老婆", "year": "1993", "date": "12-27"},
       {"name": "家公", "year": "1993", "date": "08-09"},
     ],
@@ -34,9 +34,8 @@ export const config = {
     /** 日期相关 */
 
     // 在一起的日子，格式同上
-    LOVE_DATE: "2021-08-03",
-    // 结婚纪念日
-    MARRY_DATE: "2020-01-04",
+    LOVE_DATE: "08-03",
+
 
 
     /** 好文节选 */
@@ -54,7 +53,6 @@ export const config = {
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
 // 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
